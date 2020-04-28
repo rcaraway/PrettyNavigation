2f3cbec8e44037bf288da440292c01b0bd26decc
Rob Caraway <robbor911@gmail.com>
2020-04-28 14:51:22 -0500

    build initial working version

--- Sources/PrettyNavigationBar/PrettyNavigationBar.swift
+++ Sources/PrettyNavigationBar/PrettyNavigationBar.swift
@@ -1,3 +1,24 @@
-struct PrettyNavigationBar {
-    var text = "Hello, World!"
+import UIKit
+
+extension UINavigationBar {
+    static func prettyAppearence(with color: UIColor, large: Bool = true) {
+        appearance().tintColor = .white
+        appearance().barTintColor = color
+        appearance().backgroundColor = color
+        appearance().isOpaque = true
+        appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white,
+                                            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 19, weight: .medium)]
+        appearance().prefersLargeTitles = large
+        appearance().largeTitleTextAttributes = [NSAttributedString.Key.backgroundColor : color,
+                                                 NSAttributedString.Key.foregroundColor : UIColor.white]
+    }
+    
+}
+
+class NavigationController: UINavigationController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
 }
